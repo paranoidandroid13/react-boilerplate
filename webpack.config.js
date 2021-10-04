e4c56f97f5ab185ca7c630a3c58721f5a08112b5
paranoidandroid13 <utf1313@gmail.com>
2021-10-04 23:18:52 +0300

    bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,50 @@
-/* eslint-disable no-undef */
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                }
-            },
-            {
-                test: /\.(s*)css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ]
-            },
-            // {
-            //     test: /\.css$/,
-            //     // style-loader injects CSS into the DOM using multiple <style></style>
-            //     // use: ['style-loader', 'css-loader'],
-            //     // MiniCssExtractPlugin extracts CSS into separate files
-            //     use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            // },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                loader: 'file-loader',
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        // Uncomment when have to analyze bundle composition
-        // new BundleAnalyzerPlugin({
-        //     analyzerPort: 8000,
-        //     openAnalyzer: false,
-        // }),
-        new ESLintPlugin(),
-        new MiniCssExtractPlugin({filename: 'style.css'}),
-    ],
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        port: 9000,
-        open: true,
-    },
-};
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    // new BundleAnalyzerPlugin({
+    //   analyzerPort: 8080,
+    //   openAnalyzer: true
+    // }),
+    new ESLintPlugin(),
+    new MiniCssExtractPlugin({ filename: 'style.css' })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000,
+    open: true
+  }
+}
