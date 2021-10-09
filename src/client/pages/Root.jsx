88c34d2c57d4a90d438a6d52ef07baa025dbd543
paranoidandroid13 <utf1313@gmail.com>
2021-10-09 21:07:34 +0300

    I hope this imports shit works

--- src/client/pages/Root.jsx
+++ src/client/pages/Root.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import styles from './Root.module.scss'
 
-dfgdfg
-
 const Root = (props) => {
   return <div className={styles.test}>test</div>
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Router from './client/Router'
 
-sdsdg
-
 ReactDOM.render(<Router />, document.getElementById('root'))
